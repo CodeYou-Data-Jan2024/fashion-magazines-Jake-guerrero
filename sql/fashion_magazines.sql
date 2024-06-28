b1a92f4370c89bce4b7db13f25f139e9e4586274
Jake Guerrero <jakeguerrero9@aol.com>
2024-06-28 12:40:13 -0500

    Making sure my code and csv were correct on GitHub and the example.

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,11 +1,14 @@
-SELECT *, PRINTF('$%.2!', SUM(subscriptions.subscription_length * subscriptions.price_per_month)) AS "Amount Due"
-FROM orders
-JOIN customers
-ON orders.customer_id = customers.customer_id
-
-JOIN subscriptions
-ON orders.subscription_id = subscriptions.subscription_id
-
-WHERE subscriptions.description = 'Fashion Magazine'
-AND orders.order_status = "unpaid"
-GROUP BY customers.customer_name
+SELECT
+    customers.customer_name AS Customer,
+    PRINTF('$%.2f', SUM(subscriptions.price_per_month * subscriptions.subscription_length)) AS "Amount Due"
+FROM
+    customers
+JOIN
+    orders ON customers.customer_id = orders.customer_id
+JOIN
+    subscriptions ON orders.subscription_id = subscriptions.subscription_id
+WHERE
+    orders.order_status = 'unpaid'
+    AND subscriptions.description = 'Fashion Magazine'
+GROUP BY
+    customers.customer_name;
