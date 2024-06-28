222043f95876e41d85557f018b08fe9cd48e5a28
Jake Guerrero <jakeguerrero9@aol.com>
2024-06-28 10:48:52 -0500

    Adding homework to github

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,11 @@
--- Add your sql here
+SELECT *, PRINTF(SUM(subscriptions.subscription_length * subscriptions.price_per_month))
+FROM orders
+JOIN customers
+ON orders.customer_id = customers.customer_id
+
+JOIN subscriptions
+ON orders.subscription_id = subscriptions.subscription_id
+
+WHERE subscriptions.description = 'Fashion Magazine'
+AND orders.order_status = "unpaid"
+GROUP BY customers.customer_name
